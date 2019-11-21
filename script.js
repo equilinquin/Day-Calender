ef010ff3f920ef1861788c3a89f52a44947495d5
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2019-11-21 15:29:00 -0800

    added localStorage function

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ console.log(months[d.getMonth()])
 console.log(d.getDate())
 console.log(d.getFullYear())
 
-$("#saveBtn").click(function(){
-    var event = $("textarea").text();
-    localStorage.setItem("addEvent", event);
+var btn = [
+    {btn9: $("#9am").val()},
+    {btn10: $("#10am").val()},
+    {btn11: $("#11am").val()},
+    {btn12: $("#12pm").val()},
+    {btn1: $("#1pm").val()},
+    {btn2: $("#2pm").val()},
+    {btn3: $("#3pm").val()},
+    {btn4: $("#4pm").val()},
+    {btn5: $("#5pm").val()}
+]
 
+$("#btn9").click(function(event){
+    event.preventDefault();
+
+
+    var addEvent9 = $("#9am").val();
+    localStorage.setItem("addEvent9", addEvent9);
+    console.log(addEvent9);
+    getEvent9();
 
 });
 
-$("textarea").html = localStorage.getItem("addevent");
+getEvent9();
+
+function getEvent9() {
+    var savedEvent9 = localStorage.getItem("addEvent9");
+    $("#9am").val(savedEvent9);
+}
