972d94b15da0499a1c3e6da7856f1257c180cc23
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2019-11-22 14:45:10 -0800

    final updates to files.  Removed moment.js

--- moment.js
+++
@@ -1,8 +0,0 @@
-var d = new Date();
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var currentDay = $("#currentDay")
-
-currentDay.text(months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear());
-console.log(months[d.getMonth()])
-console.log(d.getDate())
-console.log(d.getFullYear())
--- script.js
+++ script.js
@@ -1,63 +1,23 @@
-var btn = [
-    $("#btn9"),
-    $("#btn10"),
-    $("#btn11"),
-    $("#btn12"),
-    $("#btn1"),
-    $("#btn2"),
-    $("#btn3"),
-    $("#btn4"),
-    $("#btn5")
-]
+var d = new Date();
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var currentDay = $("#currentDay")
 
-var addEvent = [
-    $("#9am").val(),
-    $("#10am").val(),
-    $("#11am").val(),
-    $("#12pm").val(),
-    $("#1pm").val(),
-    $("#2pm").val(),
-    $("#3pm").val(),
-    $("#4pm").val(),
-    $("#5pm").val()
-]
+currentDay.text(months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear());
+console.log(months[d.getMonth()])
+console.log(d.getDate())
+console.log(d.getFullYear())
 
 var eventArr = ["addEvent9","addEvent10", "addEvent11", "addEvent12", "addEvent1", "addEvent2", "addEvent3", "addEvent4", "addEvent5"]
 
 
-for (var i = 0; i < btn.length; i++) {
-    btn[i].click(function(event) {
-        event.preventDefault();
 
-        for(var j = 0; j < addEvent.length; j++) {
-
-            for (var k = 0; k < eventArr.length; k++) {
-
-            if (i === j && j === k) {
-                localStorage.setItem(eventArr[k], addEvent[j]);
-            }
-        }
-    }
-     getEvent();   
-
-    })
-}
-
-getEvent();
-
-function getEvent () {
-    var savedEvent9 = localStorage.getItem(eventArr[0])
-    //     {savedEvent10 = localStorage.getItem("addEvent10")}
-    // ]
-    //for (var i = 0; i < savedEvent.length; i++) {
-        var nineAM = $("#9am");
-        //var tenAM = $("#10am");
-
-        nineAM.val(savedEvent9);
-        //tenAM.val(savedEvent10);
-    }
-
- 
+$(".saveBtn").click(function(event) {
+    event.preventDefault();
+    var textAreaId = $(this).attr("data-hour");
+    var eventVar = $(this).attr("data-addEvent");
+    localStorage.setItem(eventVar, $("#" + textAreaId).val());
+    getEvent(); 
+});
 
 // $("#btn9").click(function(event){
 //     event.preventDefault();
@@ -70,9 +30,48 @@ function getEvent () {
 
 // });
 
-// getEvent9();
+getEvent();
+function getEvent () {
+    var savedEvent9 = localStorage.getItem(eventArr[0]);
+    $("#9-am").val(savedEvent9);
+    var savedEvent10 = localStorage.getItem(eventArr[1]);
+    $("#10-am").val(savedEvent10);
+    var savedEvent11 = localStorage.getItem(eventArr[2]);
+    $("#11-am").val(savedEvent11);
+    var savedEvent12 = localStorage.getItem(eventArr[3]);
+    $("#12-pm").val(savedEvent12);
+    var savedEvent1 = localStorage.getItem(eventArr[4]);
+    $("#13-pm").val(savedEvent1);
+    var savedEvent2 = localStorage.getItem(eventArr[5]);
+    $("#14-pm").val(savedEvent2);
+    var savedEvent3 = localStorage.getItem(eventArr[6]);
+    $("#15-pm").val(savedEvent3);
+    var savedEvent4 = localStorage.getItem(eventArr[7]);
+    $("#16-pm").val(savedEvent4);
+    var savedEvent5 = localStorage.getItem(eventArr[8]);
+    $("#17-pm").val(savedEvent5);
+};
 
-// function getEvent9() {
-//     var savedEvent9 = localStorage.getItem("addEvent9");
-//     $("#9am").val(savedEvent9);
-// }
+function updateHour() {
+    var currentHour = moment().hours();
+    console.log(currentHour)
+    
+    $("textarea").each(function() {
+
+        var hourBlock = parseInt($(this).attr("id").split("-")[0]);
+        console.log(hourBlock)
+
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+        }else if (hourBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+
+};
+updateHour();
